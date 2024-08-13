debc9e46d4564ff7050f3716f558f2029c5b7268
TommAll <121292081+TommAll@users.noreply.github.com>
2024-08-13 17:33:43 +0200

    aggiunta mappa
    aggiunta mappa e marker

--- JS/script.js
+++ JS/script.js
@@ -4,10 +4,53 @@ window.onload = async()=>
     let comuni;
     let dati;
 
+
+
     let promise = await fetch("https://axqvoqvbfjpaamphztgd.functions.supabase.co/comuni?format=json&onlyname=true");
     dati=await promise.json()
     comuni=estraiComuni(dati);
     console.log(comuni); 
+
+    let coord = [parseFloat("12.4963655"), parseFloat("41.9027835")];
+
+    var map = new ol.Map({
+        //target: ID html del contenitore della mappa
+        target: 'map',
+        //livelli/strati sovrapposti sulla mappa
+        layers: [
+            //Primo livello -> mappa base
+            new ol.layer.Tile({
+                //Tipo della mappa 
+                source: new ol.source.OSM()
+            })
+        ],
+        //Rappresentazione dei layers e loro configurazione
+        view: new ol.View({
+            //coordinate del punto centrale della mappa
+            center: ol.proj.fromLonLat(coord),
+            //zoom della mappa
+            zoom: 5
+        })
+    });
+
+    const marker=new ol.layer.Vector({
+        source: new ol.source.Vector({
+            features:[
+                new ol.Feature({
+                    geometry: new ol.geom.Point(
+                        ol.proj.fromLonLat([12.4963655,41.9027835])
+                    )
+                })
+            ]
+        }),
+        style: new ol.style.Style({
+            image: new ol.style.Icon({
+                src:'icona (1).png'
+            })
+        })
+    })
+    map.addLayer(marker)
+
 
     
     
